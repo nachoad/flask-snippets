28ed9e7a732b474221cf7f44a327e59850f15996
nacho <elwanax@gmail.com>
2018-02-20 12:30:53 +0100

    updated

--- 08-un-template-con-if-for/main.py
+++ 08-un-template-con-if-for/main.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask import render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/user/<name>')
 def user(name='Nacho'):
     age = 19
     my_list = [1, 2, 3, 4]
     return render_template('user.html', nombre=name, age=age, mylist=my_list)
-
