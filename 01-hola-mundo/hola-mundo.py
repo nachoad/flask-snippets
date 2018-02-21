d771978607f703f6d6abcd5b6fafbd6ed6ce8ee4
nacho <elwanax@gmail.com>
2018-02-21 23:24:32 +0100

    updated

--- 01-hola-mundo/hola-mundo.py
+++ 01-hola-mundo/hola-mundo.py
@@ -8,3 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello world from Flask!'
+
+
+'''
+if __name__ == '__main__':
+    app.run(debug = True, port=8000)
+'''
--- 10-template-con-varios-users/main.py
+++ 10-template-con-varios-users/main.py
@@ -4,12 +4,10 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def user(name='Nacho'):
-    name = nacho
+def index():
+    name = 'nacho'
     return render_template('index.html', name=name)
 
-@app.route('/user/client')
-def client():
-    age = 19
-    my_list = [1, 2, 3, 4]
-    return render_template('user.html', nombre=name, age=age, mylist=my_list)
+
+if __name__ == '__main__':
+    app.run(debug = True, port=8000)
