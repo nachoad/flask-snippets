d0c6fc142a9261ef15b2674f536025632daa0404
nacho <elwanax@gmail.com>
2018-02-25 01:52:52 +0100

    updated

--- 14-formularios-envio-datos/main.py
+++ 14-formularios-envio-datos/main.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 import forms
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    comment_form = forms.CommentForm()
+    comment_form = forms.CommentForm(request.form)
+    if request.method == 'POST':
+        print (comment_form.username.data)
+        print (comment_form.email.data)
+        print (comment_form.comment.data)
+
     name = 'Nacho'
     return render_template('index.html', name=name, form = comment_form)
 
