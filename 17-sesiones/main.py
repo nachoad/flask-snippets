a641e0c3ccc5352c30b2e81246994a683b72e9aa
nacho <elwanax@gmail.com>
2018-03-08 22:31:32 +0100

    final update session code

--- 17-sesiones/main.py
+++ 17-sesiones/main.py
@@ -1,20 +1,41 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import url_for
+from flask import redirect
+from flask import session
 import forms
 
 app = Flask(__name__)
+app.secret_key = 'mi_clave_secreta'
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
+    # Leemos la session
+    if 'username' in session:
+        username = session['username']
+        print (username)
     name = 'Nacho'
     return render_template('index.html', name=name)
 
-@app.route('/login')
+
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
-    login_form = forms.LoginForm()
+    login_form = forms.LoginForm(request.form)
+    if request.method == 'POST' and login_form.validate():
+        # Creamos la session
+        session['username'] = login_form.username.data
 
     return render_template('login.html', form = login_form)
+
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        # Destruimos la session
+        session.pop('username')
+
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
